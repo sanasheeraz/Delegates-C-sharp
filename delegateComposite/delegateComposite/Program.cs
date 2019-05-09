9c6eee07eed610ac1ee38cd0c055c1dec056a8f5
Sana Sheeraz <sana278.a@gmail.com>
2019-05-09 11:19:50 +0500

    first commit

---
+++ delegateComposite/delegateComposite/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void Math(int value);
+namespace delegateComposite
+{
+    class Program
+    {
+        public static void  EvenNumber(int value)
+        {
+            Console.WriteLine("Even Numbers : ");
+            for(var i=2;i<value;i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void OddNumber(int value)
+        {
+            Console.WriteLine("Odd Numbers : ");
+            for (var i = 1; i < value; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+        static void Start(int num)
+        {
+            Program compositeDelegate = new Program();
+            Math One = new Math(EvenNumber);
+            Math Two = new Math(compositeDelegate.OddNumber);
+            Math objComposite = (Math)Delegate.Combine(One, Two);
+            objComposite(num);
+            Console.WriteLine();
+            Object obj = objComposite.Method.ToString();
+            if(obj!=null)
+            {
+                Console.WriteLine("The delegate invokes an instance method : " + obj);
+            }
+            else
+            {
+                Console.WriteLine("The delegate invokes only static methods " );
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            int value = 0;
+            Console.WriteLine("Enter any value till which you want to display Even and Odd Numbers : ");
+
+            try
+            {
+                value = Convert.ToInt32(Console.ReadLine());
+            }catch(FormatException e)
+            {
+                Console.WriteLine("Error : " + e);
+            }
+            Start(value);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ delegates1/delegates1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates1
+{
+    class Program
+    {
+        public delegate void Calculator(int a, int b);
+        public delegate void Calculator1(int a, int b);
+        public static void add(int a, int b)
+        {
+            Console.WriteLine("Add : " + (a + b));
+        }
+
+        public static void sub(int a, int b)
+        {
+            Console.WriteLine("Sub: " + (a - b));
+        }
+
+        public static void mul(int a, int b)
+        {
+            Console.WriteLine("Mul : " + (a * b));
+        }
+
+        public static void div(int a, int b)
+        {
+            Console.WriteLine("Divide : " + (a / b));
+        }
+        static void Main(string[] args)
+        {
+
+            Calculator calculator = new Calculator(add);
+            //calculator(2, 4);
+            calculator += new Calculator(sub);
+            calculator += new Calculator(mul);
+            calculator += new Calculator(div);
+            calculator(4, 2);
+
+            Calculator1 cal=delegate(int a,int b){Console.WriteLine(a+b);};
+            cal(2,3);
+            Console.ReadKey();
+        }
+    }
+}
